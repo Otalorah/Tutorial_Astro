9262a7963883ded151c63952ea9e70f1d775eb58
Juan Otalora <juanmaotalora15@gmail.com>
2024-03-12 11:23:29 -0500

    Implementación de layouts

---
+++ src/scripts/information.js
@@ -0,0 +1,12 @@
+export const identity = {
+    firstName: "Juan",
+    country: "Colombia",
+    occupation: "Estudiantte",
+    hobbies: ["programar", "observación de aves", "peñarol"],
+};
+
+export const skills = ["HTML", "CSS", "JavaScript", "React", "Astro", "Python"];
+
+export const happy = true;
+export const finished = false;
+export const goal = 1;
